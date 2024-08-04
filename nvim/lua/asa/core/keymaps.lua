cb857aca9928f072a5316c196e9b29ff4abdea1e
Akilesh-ML-Engineer <akilesh.ml.engineer@gmail.com>
2024-08-04 17:20:05 +0530

    Updated the keybindings!

--- nvim/lua/asa/core/keymaps.lua
+++ nvim/lua/asa/core/keymaps.lua
@@ -3,8 +3,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
----------------------
--- General Keymaps -------------------
+----------------------- General Keymaps -------------------
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
