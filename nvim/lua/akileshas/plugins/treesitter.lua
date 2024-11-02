4712705333802073c1fd18fe763bc7c07d6f3747
akileshas <akilesh.ml.engineer@gmail.com>
2024-11-03 01:22:12 +0530

    Added the treesitter!

---
+++ nvim/lua/akileshas/plugins/treesitter.lua
@@ -0,0 +1,80 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    event = { 
+        "BufReadPre", 
+        "BufNewFile" 
+    },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
+
+        -- configure treesitter
+        treesitter.setup({ -- enable syntax highlighting
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            -- enable indentation
+            indent = { 
+                enable = true 
+            },
+            -- enable autotagging (w/ nvim-ts-autotag plugin)
+            autotag = {
+                enable = true,
+            },
+            auto_install = true,
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "prisma",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "luadoc",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "query",
+                "vimdoc",
+                "c",
+                "cpp",
+                "java",
+                "go",
+                "python",
+                "rust",
+                "r",
+                "ruby",
+                "php",
+                "graphql",
+                "scss",
+                "jsonc",
+                "toml",
+                "swift",
+                "regex",
+                "svelte",
+                "vue",
+                "zig",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
+}
