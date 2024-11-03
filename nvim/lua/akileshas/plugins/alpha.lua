0a5063b7ac295f2e1f7b85fcb5472161cc514b91
akileshas <akilesh.ml.engineer@gmail.com>
2024-11-03 11:31:33 +0530

    Added the needed config!

---
+++ nvim/lua/akileshas/plugins/alpha.lua
@@ -0,0 +1,26 @@
+return {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        -- Set header
+        dashboard.section.header.val = {
+            "              _             _     _ _                          ",
+            "             / \\   _ __ ___| |__ | (_)_ __  _   ___  __       ",
+            "            / _ \\ | '__/ __| '_ \\| | | '_ \\| | | \\ \\/ /   ",
+            "           / ___ \\| | | (__| | | | | | | | | |_| |>  <        ",
+            "          /_/   \\_\\_|  \\___|_| |_|_|_|_| |_|\\__,_/_/\\_\\  ",
+        }
+
+        -- Set menu
+        dashboard.section.buttons.val = {}
+
+        -- Send config to alpha
+        alpha.setup(dashboard.opts)
+
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
+}
