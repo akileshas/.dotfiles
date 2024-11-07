296727a8b9576abffc94b64a5b1c1dc470b03ece
akileshas <akilesh.ml.engineer@gmail.com>
2024-11-07 19:28:08 +0530

    Updated the i3 config!

---
+++ i3/scripts/fan_speed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Use sensors to get fan speeds
+FAN1=$(sensors | grep -m1 'fan1' | awk '{print $2}')
+FAN2=$(sensors | grep -m1 'fan2' | awk '{print $2}')
+
+# Check if fan speeds were detected
+if [ -z "$FAN1" ]; then
+    FAN1="N/A"
+fi
+if [ -z "$FAN2" ]; then
+    FAN2="N/A"
+fi
+
+# Output fan speeds
+echo " 󱑲 : ${FAN1} RPM  󱑳 : ${FAN2} RPM "
