90bef1c742c8b5d2565327c96d7b0c236443e23b
akileshas <akilesh.ml.engineer@gmail.com>
2025-01-01 10:11:31 +0530

    Fixed some issue in tmux!

--- tmux/scripts/uptime.sh
+++ tmux/scripts/uptime.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 # Get the uptime of the system
-uptime | sed -n 's/.*up \([^,]*\),.*/\1/p'
+uptime -p | awk '{
+    days=0; hours=0; minutes=0;
+    for (i=1; i<=NF; i++) {
+        if ($i ~ /day/) days=$(i-1);
+        if ($i ~ /hour/) hours=$(i-1);
+        if ($i ~ /minute/) minutes=$(i-1);
+    }
+    printf "%02d:%02d:%02d\n", days, hours, minutes;
+}'
