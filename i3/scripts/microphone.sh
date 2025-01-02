0212a43de27072a6a8bc553903a3b30a96746d89
akileshas <akilesh.ml.engineer@gmail.com>
2025-01-02 17:06:03 +0530

    Added the notification for mic!

---
+++ i3/scripts/microphone.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check if the microphone is muted using pamixer
+if pamixer --default-source --get-mute | grep -q "true"; then
+    echo "  "
+else
+    echo ""
+fi
--- i3/scripts/notifications.sh
+++ i3/scripts/notifications.sh
@@ -5,6 +5,7 @@ current_power=$(~/.dotfiles/i3/scripts/current_power_profile.sh)
 caps_lock=$(~/.dotfiles/i3/scripts/caps_lock.sh)
 num_lock=$(~/.dotfiles/i3/scripts/num_lock.sh)
 flight_mode=$(~/.dotfiles/i3/scripts/flight_mode.sh)
+mic=$(~/.dotfiles/i3/scripts/microphone.sh)
 
 # Combine all outputs into a single line
-echo "$current_power$caps_lock$num_lock$flight_mode"
+echo "$current_power$caps_lock$num_lock$flight_mode$mic"
