d73c8eb586f66a6f69a4b5900e15591ebd0d7899
akileshas <akilesh.ml.engineer@gmail.com>
2025-01-09 21:33:37 +0530

    Fixed some issues !

--- i3/scripts/notifications.sh
+++ i3/scripts/notifications.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Run each script and capture its output
-mic=$(~/.dotfiles/i3/scripts/microphone.sh)
-current_power=$(~/.dotfiles/i3/scripts/current_power_profile.sh)
 caps_lock=$(~/.dotfiles/i3/scripts/caps_lock.sh)
 num_lock=$(~/.dotfiles/i3/scripts/num_lock.sh)
+mic=$(~/.dotfiles/i3/scripts/microphone.sh)
 flight_mode=$(~/.dotfiles/i3/scripts/flight_mode.sh)
+current_power_profile=$(~/.dotfiles/i3/scripts/current_power_profile.sh)
 
 # Combine all outputs into a single line
-echo "$mic$current_power$caps_lock$num_lock$flight_mode"
+echo "$caps_lock$num_lock$mic$flight_mode$current_power_profile"
