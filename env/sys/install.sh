b66775789bc6a2627fdb1aff0ff502fed6639029
akileshas <akileshas.ai@gmail.com>
2025-01-21 00:58:58 +0530

    Fixed some issues!

--- env/sys/install.sh
+++ env/sys/install.sh
@@ -74,25 +74,6 @@ sudo systemctl enable paccache.timer
 sudo systemctl start paccache.timer
 echo "Activated the paccache timer"
 
-# Making the required directories
-echo "Making the required directories..."
-mkdir ~/personal
-mkdir ~/work
-mkdir ~/work/gsoc
-mkdir ~/work/ggs
-mkdir ~/work/dev
-mkdir ~/documents
-mkdir ~/documents/books
-mkdir ~/documents/papers
-mkdir ~/downloads
-mkdir ~/repos
-mkdir ~/pictures
-mkdir ~/pictures/screenshots
-mkdir ~/videos
-mkdir ~/videos/obs
-mkdir ~/tmp
-echo "Created the required directories"
-
 # Creating the required symbolic links
 # For `i3`
 if [[ -d ~/.config/i3 ]]; then
---
+++ env/sys/pre-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Making the required directories
+echo "Making the required directories..."
+mkdir ~/personal
+mkdir ~/work
+mkdir ~/work/gsoc
+mkdir ~/work/ggs
+mkdir ~/work/dev
+mkdir ~/documents
+mkdir ~/documents/books
+mkdir ~/documents/papers
+mkdir ~/downloads
+mkdir ~/repos
+mkdir ~/pictures
+mkdir ~/pictures/screenshots
+mkdir ~/videos
+mkdir ~/videos/obs
+mkdir ~/tmp
+echo "Created the required directories"
