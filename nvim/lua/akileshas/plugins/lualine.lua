c4218a2b136868a74d7c738848eb5a17ca82d3e8
akileshas <akileshas.ai@gmail.com>
2025-02-25 09:02:58 +0530

    Fixed some issues!

--- nvim/lua/akileshas/plugins/lualine.lua
+++ nvim/lua/akileshas/plugins/lualine.lua
@@ -3,17 +3,11 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
 		lualine.setup({
 			sections = {
 				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-					},
 					{ "encoding" },
-					-- { "fileformat" },
 					{ "filetype" },
 				},
 			},
