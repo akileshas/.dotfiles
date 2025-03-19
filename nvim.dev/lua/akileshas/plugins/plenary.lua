7e6791a3767b1eeceb614466677dcb3cb696e0fa
akileshas <akileshas.ai@gmail.com>
2025-03-19 15:13:34 +0530

    added the plenary.nvim plugin!

---
+++ nvim.dev/lua/akileshas/plugins/plenary.lua
@@ -0,0 +1,13 @@
+-- plugin options
+local options = {}
+
+-- plugin keymaps
+local keymaps = {}
+
+-- plugins configuration
+return {
+	"nvim-lua/plenary.nvim",
+	lazy = true,
+	opts = options,
+	keys = keymaps,
+}
