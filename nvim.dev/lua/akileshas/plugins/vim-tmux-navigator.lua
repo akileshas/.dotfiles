c2c700b17a0bff7bf62e9939026c4d9f548545b1
akileshas <akileshas.ai@gmail.com>
2025-03-19 15:14:02 +0530

    added the vim-tmux-navigator plugin!

---
+++ nvim.dev/lua/akileshas/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,13 @@
+-- plugin options
+local options = {}
+
+-- plugin keymaps
+local keymaps = {}
+
+-- plugins configuration
+return {
+	"christoomey/vim-tmux-navigator",
+	lazy = true,
+	opts = options,
+	keys = keymaps,
+}
