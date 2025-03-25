2750a83b52f82ac46c25dbdc68663394acd014eb
akileshas <akileshas.ai@gmail.com>
2025-03-26 01:28:51 +0530

    updated the color schema!

--- nvim/lua/akileshas/plugins/colorschema.lua
+++ nvim/lua/akileshas/plugins/colorschema.lua
@@ -3,8 +3,32 @@ local options = {
 	style = "deep",
 	transparent = true,
 	term_colors = true,
+	ending_tildes = false,
+	cmp_itemkind_reverse = false,
+	toggle_style_key = "<leader>cs",
+	toggle_style_list = {
+		'dark',
+		'darker',
+		'cool',
+		'warm',
+		'warmer',
+		'deep',
+		'light',
+	},
+	code_style = {
+		comments = 'italic',
+		keywords = 'none',
+		functions = 'none',
+		strings = 'none',
+		strings = 'none',
+	},
 	lualine = {
-		transparent = true,
+		transparent = false,
+	},
+	diagnostics = {
+		darker = true,
+		undercurl = true,
+		background = true,
 	},
 }
 
