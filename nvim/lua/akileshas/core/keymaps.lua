072b4bc711ddaee9dcfda8a208be908630168671
akileshas <akileshas.ai@gmail.com>
2025-03-26 19:08:27 +0530

    fixed the keymaps!

--- nvim/lua/akileshas/core/keymaps.lua
+++ nvim/lua/akileshas/core/keymaps.lua
@@ -9,67 +9,67 @@ keymap.set("i", "jj", "<Esc>", {
 	desc = "exit insert mode with jj",
 })
 
-keymap.set("n", "<leader>cl", "<cmd>nohlsearch<CR>", {
+keymap.set({ "n", "v", "x" }, "<leader>cl", "<cmd>nohlsearch<CR>", {
 	noremap = true,
 	silent = true,
 	desc = "clear search highlights",
 })
 
-keymap.set("n", "<leader>=", "<C-a>", {
+keymap.set({ "n", "v", "x" }, "=", "<C-a>", {
 	noremap = true,
 	silent = true,
 	desc = "Increment number",
 })
 
-keymap.set("n", "<leader>-", "<C-x>", {
+keymap.set({ "n", "v", "x" }, "-", "<C-x>", {
 	noremap = true,
 	silent = true,
 	desc = "Decrement number",
 })
 
-keymap.set("n", "<leader>sv", "<C-w>v", {
+keymap.set({ "n", "v", "x" }, "<leader>sv", "<C-w>v", {
 	noremap = true,
 	silent = true,
 	desc = "Split window vertically",
 })
 
-keymap.set("n", "<leader>sh", "<C-w>s", {
+keymap.set({ "n", "v", "x" }, "<leader>sh", "<C-w>s", {
 	noremap = true,
 	silent = true,
 	desc = "Split window horizontally",
 })
 
-keymap.set("n", "<leader>se", "<C-w>=", {
+keymap.set({ "n", "v", "x" }, "<leader>se", "<C-w>=", {
 	noremap = true,
 	silent = true,
 	desc = "Make splits equal size",
 })
 
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", {
+keymap.set({ "n", "v", "x" }, "<leader>sx", "<cmd>close<CR>", {
 	noremap = true,
 	silent = true,
 	desc = "Close current split",
 })
 
-keymap.set("n", "<C-Left>", [[<cmd>vertical resize +5<CR>]], {
+keymap.set({ "n", "v", "x" }, "<C-Left>", [[<cmd>vertical resize +5<CR>]], {
 	noremap = true,
 	silent = true,
 	desc = "Make the window bigger vertically",
 })
 
-keymap.set("n", "<C-Right>", [[<cmd>vertical resize -5<CR>]], {
+keymap.set({ "n", "v", "x" }, "<C-Right>", [[<cmd>vertical resize -5<CR>]], {
 	noremap = true,
 	silent = true,
 	desc = "Make the window smaller vertically",
 })
 
-keymap.set("n", "<C-Up>", [[<cmd>horizontal resize +2<CR>]], {
+keymap.set({ "n", "v", "x" }, "<C-Up>", [[<cmd>horizontal resize +2<CR>]], {
 	noremap = true,
 	silent = true,
 	desc = "Make the window bigger horizontally",
 })
 
-keymap.set("n", "<C-Down>", [[<cmd>horizontal resize -2<CR>]], {
+keymap.set({ "n", "v", "x" }, "<C-Down>", [[<cmd>horizontal resize -2<CR>]], {
 	noremap = true,
 	silent = true,
 	desc = "Make the window smaller horizontally",
