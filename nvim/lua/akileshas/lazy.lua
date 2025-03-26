ebda12527e33d64206a4f46328141a02e353a207
akileshas <akileshas.ai@gmail.com>
2025-03-26 17:08:40 +0530

    refractor the lazy.nvim plugin manager!

--- nvim/lua/akileshas/lazy.lua
+++ nvim/lua/akileshas/lazy.lua
@@ -1,33 +1,51 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 -- for convenience
 local fn = vim.fn
 local loop = vim.loop
+local uv = vim.uv
+local v = vim.v
+local api = vim.api
 local opt = vim.opt
 
-if not loop.fs_stat(lazypath) then
-	fn.system({
+-- bootstrap lazy.nvim
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (uv or loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
-		lazypath,
+		lazyrepo,
+		lazypath
 	})
+	if v.shell_error ~= 0 then
+		api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		fn.getchar()
+		os.exit(1)
+	end
 end
-
 opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	{ import = "akileshas.plugins" },
-	{ import = "akileshas.lsp" },
-}, {
+-- lazy options
+local options = {
+	spec = {
+		{ import = "akileshas.plugins" },
+		{ import = "akileshas.lsp" },
+	},
 	checker = {
+		enabled = true,
 		notify = false,
 	},
 	change_detection = {
+		enabled = true,
 		notify = false,
 	},
-})
+}
+
+require("lazy").setup(options)
 
 return {}
