7f45f8ed23bec3c1dbeb6f87e7295523702d01be
akileshas <akileshas.ai@gmail.com>
2025-03-27 19:08:42 +0530

    added duck.nvim plugin!

---
+++ nvim/lua/akileshas/plugins/duck.lua
@@ -0,0 +1,74 @@
+-- plugin options
+local options = {}
+
+-- plugin keymaps
+local keymaps = {
+	{
+		"<leader>dh",
+		mode = "n",
+		function()
+			require("duck").hatch("🐤")
+		end,
+		noremap = true,
+		silent = true,
+		desc = "hatch a hen",
+	},
+	{
+		"<leader>dd",
+		mode = "n",
+		function()
+			require("duck").hatch("🦆")
+		end,
+		noremap = true,
+		silent = true,
+		desc = "hatch a duck",
+	},
+	{
+		"<leader>dc",
+		mode = "n",
+		function()
+			require("duck").hatch("🐈")
+		end,
+		noremap = true,
+		silent = true,
+		desc = "hatch a cat",
+	},
+	{
+		"<leader>dr",
+		mode = "n",
+		function()
+			require("duck").hatch("🦀")
+		end,
+		noremap = true,
+		silent = true,
+		desc = "hatch a rust",
+	},
+	{
+		"<leader>dk",
+		mode = "n",
+		function()
+			require("duck").cook()
+		end,
+		noremap = true,
+		silent = true,
+		desc = "cook one of them",
+	},
+	{
+		"<leader>da",
+		mode = "n",
+		function()
+			require("duck").cook_all()
+		end,
+		noremap = true,
+		silent = true,
+		desc = "cook all",
+	},
+}
+
+-- plugin configurations
+return {
+	"tamton-aquib/duck.nvim",
+	lazy = true,
+	opts = options,
+	keys = keymaps,
+}
