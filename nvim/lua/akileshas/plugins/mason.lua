2b17749984e9d1fd1f1e650856137cccbdef4fbd
akileshas <akileshas.ai@gmail.com>
2025-03-31 17:12:04 +0530

    fixed some issue!

--- nvim/lua/akileshas/plugins/mason.lua
+++ nvim/lua/akileshas/plugins/mason.lua
@@ -21,12 +21,26 @@ local init = function() end
 
 -- plugin opts
 local opts = {
+	max_concurrent_installers = 8,
 	ui = {
 		check_outdated_packages_on_open = true,
 		icons = {
 			package_installed = "✓",
 			package_pending = "➜",
 			package_uninstalled = "✗",
+		},
+		keymaps = {
+			toggle_package_expand = "<CR>",
+			install_package = "i",
+			update_package = "u",
+			check_package_version = "c",
+			update_all_packages = "U",
+			check_outdated_packages = "C",
+			uninstall_package = "X",
+			cancel_installation = "<C-c>",
+			apply_language_filter = "<C-f>",
+			toggle_package_install_log = "<CR>",
+			toggle_help = "g?",
 		},
 	},
 }
