46e16b369a9b552290a7269c69fe700a62f2d359
akileshas <akileshas.ai@gmail.com>
2025-04-05 02:14:32 +0530

    fixed some issues!

--- nvim/lua/akileshas/plugins/noice.lua
+++ nvim/lua/akileshas/plugins/noice.lua
@@ -14,7 +14,7 @@ local opts = {
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = false,
+			["cmp.entry.get_documentation"] = true,
 		},
 	},
 	presets = {
