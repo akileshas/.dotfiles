f4cdd9e9e9f6d0b20585c6693cd44de10727d7b9
akileshas <akileshas.ai@gmail.com>
2025-04-06 17:45:44 +0530

    cleaned up the code!

--- nvim.old/plugins/treesitter-playground.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"nvim-treesitter/playground",
-	config = function()
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>ts", "<cmd>TSPlaygroundToggle<CR>", { desc = "Toggle Treesitter Playground" })
-	end,
-}
