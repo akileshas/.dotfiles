caff5b1ef6cb0f0ebc09e38398523bc150e1b5b3
akileshas <akileshas.ai@gmail.com>
2025-04-14 03:19:43 +0530

    fixed some issues!

--- nvim.fix/duck.lua
+++ nvim.fix/duck.lua
@@ -3,72 +3,72 @@ local options = {}
 
 -- plugin keymaps
 local keymaps = {
-	{
-		"<leader>dh",
-		mode = "n",
-		function()
-			require("duck").hatch("🐤")
-		end,
-		noremap = true,
-		silent = true,
-		desc = "hatch a hen",
-	},
-	{
-		"<leader>dd",
-		mode = "n",
-		function()
-			require("duck").hatch("🦆")
-		end,
-		noremap = true,
-		silent = true,
-		desc = "hatch a duck",
-	},
-	{
-		"<leader>dc",
-		mode = "n",
-		function()
-			require("duck").hatch("🐈")
-		end,
-		noremap = true,
-		silent = true,
-		desc = "hatch a cat",
-	},
-	{
-		"<leader>dr",
-		mode = "n",
-		function()
-			require("duck").hatch("🦀")
-		end,
-		noremap = true,
-		silent = true,
-		desc = "hatch a rust",
-	},
-	{
-		"<leader>dk",
-		mode = "n",
-		function()
-			require("duck").cook()
-		end,
-		noremap = true,
-		silent = true,
-		desc = "cook one of them",
-	},
-	{
-		"<leader>da",
-		mode = "n",
-		function()
-			require("duck").cook_all()
-		end,
-		noremap = true,
-		silent = true,
-		desc = "cook all",
-	},
+    {
+        "<leader>dh",
+        mode = "n",
+        function()
+            require("duck").hatch("🐤")
+        end,
+        noremap = true,
+        silent = true,
+        desc = "hatch a hen",
+    },
+    {
+        "<leader>dd",
+        mode = "n",
+        function()
+            require("duck").hatch("🦆")
+        end,
+        noremap = true,
+        silent = true,
+        desc = "hatch a duck",
+    },
+    {
+        "<leader>dc",
+        mode = "n",
+        function()
+            require("duck").hatch("🐈")
+        end,
+        noremap = true,
+        silent = true,
+        desc = "hatch a cat",
+    },
+    {
+        "<leader>dr",
+        mode = "n",
+        function()
+            require("duck").hatch("🦀")
+        end,
+        noremap = true,
+        silent = true,
+        desc = "hatch a rust",
+    },
+    {
+        "<leader>dk",
+        mode = "n",
+        function()
+            require("duck").cook()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "cook one of them",
+    },
+    {
+        "<leader>da",
+        mode = "n",
+        function()
+            require("duck").cook_all()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "cook all",
+    },
 }
 
 -- plugin configurations
 return {
-	"tamton-aquib/duck.nvim",
-	lazy = true,
-	opts = options,
-	keys = keymaps,
+    "tamton-aquib/duck.nvim",
+    lazy = true,
+    opts = options,
+    keys = keymaps,
 }
