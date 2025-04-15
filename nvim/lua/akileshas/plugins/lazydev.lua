5cd60eedf0cfc5502af5c18932332402d43788a2
akileshas <akileshas.ai@gmail.com>
2025-04-16 04:36:22 +0530

    updated the lazydev.nvim plugin's library!

--- nvim/lua/akileshas/plugins/lazydev.lua
+++ nvim/lua/akileshas/plugins/lazydev.lua
@@ -5,7 +5,26 @@ local dependencies = {}
 local init = function() end
 
 -- plugin opts
-local opts = {}
+local opts = {
+    library = {
+        {
+            path = "${3rd}/luv/library",
+            words = { "vim%.uv" }
+        },
+        {
+            path = "LazyVim",
+            words = { "LazyVim" }
+        },
+        {
+            path = "snacks.nvim",
+            words = { "Snacks" }
+        },
+        {
+            path = "lazy.nvim",
+            words = { "LazyVim" }
+        },
+    },
+}
 
 -- plugin keys
 local keys = {}
