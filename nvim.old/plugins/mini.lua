bd433dabad1152553d24dbf010beab8ff67f3a49
akileshas <akileshas.ai@gmail.com>
2025-04-16 04:49:34 +0530

    cleaned up the code!

--- nvim.old/plugins/mini.lua
+++ nvim.old/plugins/mini.lua
@@ -1,38 +1,32 @@
 return {
-	{
-		"echasnovski/mini.ai",
-		config = function()
-			require("mini.ai").setup({ n_lines = 10000000000 })
-		end,
-	},
-	{
-		"echasnovski/mini.comment",
-		config = function()
-			require("mini.comment").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.jump",
-		config = function()
-			require("mini.jump").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.pairs",
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.splitjoin",
-		config = function()
-			require("mini.splitjoin").setup({ mappings = { toggle = "sj" } })
-		end,
-	},
-	{
-		"echasnovski/mini.surround",
-		config = function()
-			require("mini.surround").setup()
-		end,
-	},
+    {
+        "echasnovski/mini.comment",
+        config = function()
+            require("mini.comment").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.jump",
+        config = function()
+            require("mini.jump").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.pairs",
+        config = function()
+            require("mini.pairs").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.splitjoin",
+        config = function()
+            require("mini.splitjoin").setup({ mappings = { toggle = "sj" } })
+        end,
+    },
+    {
+        "echasnovski/mini.surround",
+        config = function()
+            require("mini.surround").setup()
+        end,
+    },
 }
