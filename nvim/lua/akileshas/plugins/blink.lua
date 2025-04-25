194c917bdc6453b898f1e4f0a2499938fab88245
akileshas <akileshas.ai@gmail.com>
2025-04-26 02:41:24 +0530

    fixed some issues in blink.cmp!

--- nvim/lua/akileshas/plugins/blink.lua
+++ nvim/lua/akileshas/plugins/blink.lua
@@ -99,9 +99,9 @@ local opts = {
             "omni",
             "emoji",
             "snippets",
+            "copilot",
             "path",
             "buffer",
-            "copilot",
         },
         providers = {
             lazydev = {
@@ -145,6 +145,22 @@ local opts = {
                 score_offset = 80,
                 max_items = 15,
             },
+            copilot = {
+                name = "copilot",
+                enabled = true,
+                module = "blink-cmp-copilot",
+                score_offset = 15,
+                async = true,
+                transform_items = function(_, items)
+                    local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                    local kind_idx = #CompletionItemKind + 1
+                    CompletionItemKind[kind_idx] = "Copilot"
+                    for _, item in ipairs(items) do
+                        item.kind = kind_idx
+                    end
+                    return items
+                end,
+            },
             path = {
                 name = "path",
                 enabled = true,
@@ -166,22 +182,6 @@ local opts = {
                 module = "blink.cmp.sources.buffer",
                 score_offset = 5,
                 max_items = 5,
-            },
-            copilot = {
-                name = "copilot",
-                enabled = true,
-                module = "blink-cmp-copilot",
-                score_offset = 15,
-                async = true,
-                transform_items = function(_, items)
-                    local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-                    local kind_idx = #CompletionItemKind + 1
-                    CompletionItemKind[kind_idx] = "Copilot"
-                    for _, item in ipairs(items) do
-                        item.kind = kind_idx
-                    end
-                    return items
-                end,
             },
         },
     },
