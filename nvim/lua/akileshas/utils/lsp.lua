98aafaa72bb438844c7426e57bf1f0dc23aa8028
akileshas <akileshas.ai@gmail.com>
2025-05-09 00:46:21 +0530

    updated the lsp utils!

--- nvim/lua/akileshas/utils/lsp.lua
+++ nvim/lua/akileshas/utils/lsp.lua
@@ -11,7 +11,23 @@ M.is_enabled = function (server)
 end
 
 M.disable = function (server, cond)
-    local def = lspconfig.configs[server]
+    local defn = lspconfig.configs[server]
+
+    if not defn or not defn.document_config then
+        return
+    end
+
+    local org_on_new_config = defn.document_config.on_new_config
+
+    defn.document_config.on_new_config = function (config, root_dir)
+        if cond(root_dir, config) then
+            config.enabled = false
+        end
+
+        if org_on_new_config then
+            org_on_new_config(config, root_dir)
+        end
+    end
 end
 
 return M
