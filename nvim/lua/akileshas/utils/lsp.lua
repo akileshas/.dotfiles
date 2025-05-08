c7881653c44561fd8404088824009541edd3a068
akileshas <akileshas.ai@gmail.com>
2025-05-08 23:51:01 +0530

    added the lsp utils!

---
+++ nvim/lua/akileshas/utils/lsp.lua
@@ -0,0 +1,17 @@
+-- for convenience
+local lspconfig = require("lspconfig")
+local lspconfig_util = require("lspconfig.util")
+
+local M = {}
+
+M.is_enabled = function(server)
+    local c = lspconfig.configs[server]
+
+    return c and c.enabled ~= false
+end
+
+M.disable = function(server, cond )
+    local def = lspconfig.configs[server]
+end
+
+return M
