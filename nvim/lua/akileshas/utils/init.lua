b8c7c4451f1d86df4de5fbcba3bbfb4d824df137
akileshas <akileshas.ai@gmail.com>
2025-05-10 02:14:13 +0530

    updated the utils!

--- nvim/lua/akileshas/utils/init.lua
+++ nvim/lua/akileshas/utils/init.lua
@@ -5,11 +5,34 @@ local M = {}
 
 M.map = function (mode, lhs, rhs, opts)
     opts = vim.tbl_deep_extend("force", {
-        silent = true,
         noremap = true,
+        silent = true,
     }, opts or {})
 
     keymap.set(mode, lhs, rhs, opts)
+end
+
+M.map_all = function (keymaps)
+    for _, key in ipairs(keymaps) do
+        local mode = key.mode or { "n" }
+        local lhs = key[1]
+        local rhs = nil
+        local opts = {}
+
+        if type(key[2]) == "function" or type(key[2]) == "string" then
+            rhs = key[2]
+        else
+            rhs = key[3]
+        end
+
+        for k, v in pairs(key) do
+            if type(k) == "string" and k ~= "mode" then
+                opts[k] = v
+            end
+        end
+
+        M.map(mode, lhs, rhs, opts)
+    end
 end
 
 return M
