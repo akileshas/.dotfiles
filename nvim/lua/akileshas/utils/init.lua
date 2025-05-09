d725f8bcb57c0a7679e97e8a3a8dae23656b412a
akileshas <akileshas.ai@gmail.com>
2025-05-10 01:40:58 +0530

    updated the utils!

--- nvim/lua/akileshas/utils/init.lua
+++ nvim/lua/akileshas/utils/init.lua
@@ -1,3 +1,15 @@
+-- for convenience
+local keymap = vim.keymap
+
 local M = {}
+
+M.map = function (mode, lhs, rhs, opts)
+    opts = vim.tbl_deep_extend("force", {
+        silent = true,
+        noremap = true,
+    }, opts or {})
+
+    keymap.set(mode, lhs, rhs, opts)
+end
 
 return M
