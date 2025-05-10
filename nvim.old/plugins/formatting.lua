70963845354fce9202e6255697de017f7d08ff65
akileshas <akileshas.ai@gmail.com>
2025-05-10 22:30:39 +0530

    cleaned up the repo!

--- nvim.old/plugins/formatting.lua
+++ nvim.old/plugins/formatting.lua
@@ -1,58 +1,58 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				-- JavaScript, TypeScript, JSX, TSX, CSS, HTML, JSON, YAML, Markdown, Liquid
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				liquid = { "prettier" },
+        conform.setup({
+            formatters_by_ft = {
+                -- JavaScript, TypeScript, JSX, TSX, CSS, HTML, JSON, YAML, Markdown, Liquid
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                liquid = { "prettier" },
 
-				-- Lua
-				lua = { "stylua" },
+                -- Lua
+                lua = { "stylua" },
 
-				-- Python
-				python = {
-					"isort",
-					"black",
-					"autopep8",
-				},
+                -- Python
+                python = {
+                    "isort",
+                    "black",
+                    "autopep8",
+                },
 
-				-- Go
-				go = {
-					"golines",
-					"goimports",
-					"gofumpt",
-				},
+                -- Go
+                go = {
+                    "golines",
+                    "goimports",
+                    "gofumpt",
+                },
 
-				-- Rust
-				rust = { "rustfmt" },
+                -- Rust
+                rust = { "rustfmt" },
 
-				-- Shell scripts
-				sh = { "shfmt" },
+                -- Shell scripts
+                sh = { "shfmt" },
 
-				-- Haskell
-				haskell = { "ormolu" },
-			},
-		})
+                -- Haskell
+                haskell = { "ormolu" },
+            },
+        })
 
-		-- Keybinding to format the current file or selected range (visual mode)
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = true,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+        -- Keybinding to format the current file or selected range (visual mode)
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({
+                lsp_fallback = true,
+                async = true,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
--- nvim.old/plugins/treesitter.lua
+++ nvim.old/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
 
-		-- configure treesitter
-		treesitted.setup({
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enabe = true,
-			},
-		})
-	end,
+        -- configure treesitter
+        treesitted.setup({
+            -- enable autotagging (w/ nvim-ts-autotag plugin)
+            autotag = {
+                enabe = true,
+            },
+        })
+    end,
 }
