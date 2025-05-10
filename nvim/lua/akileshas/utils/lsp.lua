27c87f81408c3122c9732d4166022172debc5b02
akileshas <akileshas.ai@gmail.com>
2025-05-10 19:50:19 +0530

    fixed some fixes !

--- nvim/lua/akileshas/utils/lsp.lua
+++ nvim/lua/akileshas/utils/lsp.lua
@@ -2,13 +2,10 @@
 local api = vim.api
 local lsp = vim.lsp
 
-local utils = require("akileshas.utils")
-
 local M = {}
 
 M.on_attach = function (on_attach, name)
     return api.nvim_create_autocmd({ "LspAttach" }, {
-        group = utils.augroup("lsp_attach"),
         callback = function (args)
             local buffer = args.buf
             local client = lsp.get_client_by_id(args.data.client_id)
