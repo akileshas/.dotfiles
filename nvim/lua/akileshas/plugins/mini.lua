45a3570a0f8e134e850fc77b8877e3d5d9b7c6a0
akileshas <akileshas.ai@gmail.com>
2025-05-15 03:20:23 +0530

    added the mini.move plugin!

--- nvim/lua/akileshas/plugins/mini.lua
+++ nvim/lua/akileshas/plugins/mini.lua
@@ -3,6 +3,7 @@ local dependencies = {
     edit = {
         ai = {},
         comment = {},
+        move = {},
     },
     flow = {},
     ui = {},
@@ -14,6 +15,7 @@ local init = {
     edit = {
         ai = function () end,
         comment = function () end,
+        move = function () end,
     },
     flow = {},
     ui = {},
@@ -50,6 +52,21 @@ local opts = {
                 comment_line = "gcc",
                 comment_visual = "gc",
                 textobject = "gc",
+            },
+        },
+        move = {
+            mappings = {
+                left = "<M-h>",
+                right = "<M-l>",
+                down = "<M-j>",
+                up = "<M-k>",
+                line_left = "<M-h>",
+                line_right = "<M-l>",
+                line_down = "<M-j>",
+                line_up = "<M-k>",
+            },
+            options = {
+                reindent_linewise = true,
             },
         },
     },
@@ -121,6 +138,48 @@ local keys = {
     edit = {
         ai = {},
         comment = {},
+        move = {
+            {
+                "<M-h>",
+                mode = { "i" },
+                function ()
+                    MiniMove.move_line("left")
+                end,
+                noremap = true,
+                silent = true,
+                desc = "move line left",
+            },
+            {
+                "<M-l>",
+                mode = { "i" },
+                function ()
+                    MiniMove.move_line("right")
+                end,
+                noremap = true,
+                silent = true,
+                desc = "move line right",
+            },
+            {
+                "<M-j>",
+                mode = { "i" },
+                function ()
+                    MiniMove.move_line("down")
+                end,
+                noremap = true,
+                silent = true,
+                desc = "move line down",
+            },
+            {
+                "<M-k>",
+                mode = { "i" },
+                function ()
+                    MiniMove.move_line("up")
+                end,
+                noremap = true,
+                silent = true,
+                desc = "move line up",
+            },
+        },
     },
     flow = {},
     ui = {},
@@ -163,6 +222,22 @@ return {
             init = init.edit.comment,
             opts = opts.edit.comment,
             keys = keys.edit.comment,
+        },
+        {
+            "echasnovski/mini.move",
+            version = "*",
+            enabled = true,
+            lazy = true,
+            event = {
+                "VeryLazy",
+            },
+            cmd = {},
+            ft = {},
+            build = {},
+            dependencies = dependencies.edit.move,
+            init = init.edit.move,
+            opts = opts.edit.move,
+            keys = keys.edit.move,
         },
     },
 
