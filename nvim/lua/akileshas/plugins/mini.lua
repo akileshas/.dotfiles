78d439f7c0639abb526ec63dbc90c9bb9af1de3c
akileshas <akileshas.ai@gmail.com>
2025-05-15 03:42:51 +0530

    rewriting the mini.nvim config!

---
+++ nvim/lua/akileshas/plugins/mini.lua
@@ -0,0 +1,54 @@
+-- plugin dependencies
+local dependencies = {
+    edit = {},
+    flow = {},
+    ui = {},
+    etc = {},
+}
+
+-- plugin init function
+local init = {
+    edit = {},
+    flow = {},
+    ui = {},
+    etc = {},
+}
+
+-- plugin opts
+local opts = {
+    edit = {},
+    flow = {},
+    ui = {},
+    etc = {},
+}
+
+-- plugin config function
+local config = {
+    edit = {},
+    flow = {},
+    ui = {},
+    etc = {},
+}
+
+-- plugin keys
+local keys = {
+    edit = {},
+    flow = {},
+    ui = {},
+    etc = {},
+}
+
+-- plugin configurations
+return {
+    -- text editing
+    {},
+
+    -- general workflow
+    {},
+
+    -- appearance
+    {},
+
+    -- other
+    {},
+}
