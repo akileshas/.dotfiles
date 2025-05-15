654302620141ece3dc043d554976f38aa5a5db68
akileshas <akileshas.ai@gmail.com>
2025-05-16 01:01:32 +0530

    updated the mini.pairs plugin!

--- nvim/lua/akileshas/plugins/mini.lua
+++ nvim/lua/akileshas/plugins/mini.lua
@@ -100,6 +100,56 @@ local opts = {
             },
         },
         pairs = {
+            mappings = {
+                ["("] = {
+                    action = "open",
+                    pair = "()",
+                    neigh_pattern = "[^\\].",
+                },
+                ["["] = {
+                    action = "open",
+                    pair = "[]",
+                    neigh_pattern = "[^\\].",
+                },
+                ["{"] = {
+                    action = "open",
+                    pair = "{}",
+                    neigh_pattern = "[^\\].",
+                },
+                [")"] = {
+                    action = "close",
+                    pair = "()",
+                    neigh_pattern = "[^\\].",
+                },
+                ["]"] = {
+                    action = "close",
+                    pair = "[]",
+                    neigh_pattern = "[^\\].",
+                },
+                ["}"] = {
+                    action = "close",
+                    pair = "{}",
+                    neigh_pattern = "[^\\].",
+                },
+                ['"'] = {
+                    action = "closeopen",
+                    pair = '""',
+                    neigh_pattern = "[^\\].",
+                    register = { cr = false },
+                },
+                ["'"] = {
+                    action = "closeopen",
+                    pair = "''",
+                    neigh_pattern = "[^%a\\].",
+                    register = { cr = false },
+                },
+                ["`"] = {
+                    action = "closeopen",
+                    pair = "``",
+                    neigh_pattern = "[^\\].",
+                    register = { cr = false },
+                },
+            },
             modes = {
                 insert = true,
                 command = true,
