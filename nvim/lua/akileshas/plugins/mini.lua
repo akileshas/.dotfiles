3df313df164f3fea97425e7f21b2d13271172c34
akileshas <akileshas.ai@gmail.com>
2025-05-17 02:06:51 +0530

    updated the mini.bracketed plugin!

--- nvim/lua/akileshas/plugins/mini.lua
+++ nvim/lua/akileshas/plugins/mini.lua
@@ -209,7 +209,64 @@ local opts = {
         },
     },
     flow = {
-        bracketed = {},
+        bracketed = {
+            buffer = {
+                suffix = "b",
+                options = {},
+            },
+            comment = {
+                suffix = "c",
+                options = {},
+            },
+            conflict = {
+                suffix = "x",
+                options = {},
+            },
+            diagnostic = {
+                suffix = "d",
+                options = {},
+            },
+            file = {
+                suffix = "f",
+                options = {},
+            },
+            indent = {
+                suffix = "i",
+                options = {},
+            },
+            jump = {
+                suffix = "j",
+                options = {},
+            },
+            location = {
+                suffix = "l",
+                options = {},
+            },
+            oldfile = {
+                suffix = "o",
+                options = {},
+            },
+            quickfix = {
+                suffix = "q",
+                options = {},
+            },
+            treesitter = {
+                suffix = "s",
+                options = {},
+            },
+            undo = {
+                suffix = "u",
+                options = {},
+            },
+            window = {
+                suffix = "w",
+                options = {},
+            },
+            yank = {
+                suffix = "y",
+                options = {},
+            },
+        },
     },
     ui = {
         trailspace = {
