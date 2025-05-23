9838c63ffbf6fd859a03cc3e42d4e5a779389522
akileshas <akileshas.ai@gmail.com>
2025-05-23 20:37:20 +0530

    fixed some issues!

--- nvim/lua/akileshas/plugins/noice.lua
+++ nvim/lua/akileshas/plugins/noice.lua
@@ -23,7 +23,7 @@ local opts = {
     presets = {
         bottom_search = true,
         command_palette = true,
-        long_message_to_split = true,
+        long_message_to_split = false,
         inc_rename = false,
         lsp_doc_border = true,
     },
