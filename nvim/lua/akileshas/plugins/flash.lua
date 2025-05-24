5d0acc4e5e3a7c16063de865a201e3c577dab9c2
akileshas <akileshas.ai@gmail.com>
2025-05-25 02:05:50 +0530

    update the flash.nvim config!

--- nvim/lua/akileshas/plugins/flash.lua
+++ nvim/lua/akileshas/plugins/flash.lua
@@ -8,7 +8,68 @@ local init = function () end
 local opts = {}
 
 -- plugin keys
-local keys = {}
+local keys = {
+    {
+        ",fs",
+        mode = { "n", "x", "o" },
+        function ()
+            local flash = require("flash")
+
+            flash.jump()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "flash jump",
+    },
+    {
+        ",fS",
+        mode = { "n", "x", "o" },
+        function ()
+            local flash = require("flash")
+
+            flash.treesitter_search()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "flash treesitter search",
+    },
+    {
+        ",ft",
+        mode = { "n", "x", "o" },
+        function ()
+            local flash = require("flash")
+
+            flash.treesitter()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "flash treesitter",
+    },
+    {
+        "r",
+        mode = { "o" },
+        function ()
+            local flash = require("flash")
+
+            flash.remote()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "flash remote",
+    },
+    {
+        "<C-z>",
+        mode = { "c" },
+        function ()
+            local flash = require("flash")
+
+            flash.toggle()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "toggle flash search",
+    },
+}
 
 -- plugin configurations
 return {
