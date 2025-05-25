d54d601ce2a65a93ad4cf5d352be03a2bc11b837
akileshas <akileshas.ai@gmail.com>
2025-05-25 23:40:40 +0530

    update the flash.nvim config!

--- nvim/lua/akileshas/plugins/flash.lua
+++ nvim/lua/akileshas/plugins/flash.lua
@@ -150,6 +150,76 @@ local opts = {
 -- plugin keys
 local keys = {
     {
+        ",fa",
+        mode = { "n", "x", "o" },
+        function ()
+            local flash = require("flash")
+
+            local format = function (opts)
+                return {
+                    {
+                        opts.match.label1,
+                        "FlashMatch",
+                    },
+                    {
+                        opts.match.label2,
+                        "FlashLabel",
+                    },
+                }
+            end
+
+            flash.jump({
+                search = {
+                    mode = "search",
+                },
+                label = {
+                    uppercase = false,
+                    after = false,
+                    before = { 0, 0 },
+                    style = "overlay",
+                    format = format,
+                },
+                pattern = [[\<]],
+                action = function (match, state)
+                    state:hide()
+                    flash.jump({
+                        search = {
+                            max_length = 0,
+                        },
+                        highlight = {
+                            matches = false,
+                        },
+                        label = {
+                            format = format,
+                        },
+                        matcher = function (win)
+                            return vim.tbl_filter(function (m)
+                                return m.label == match.label and m.win == win
+                            end, state.results)
+                        end,
+                        labeler = function (matches)
+                            for _, m in ipairs(matches) do
+                                m.label = m.label2
+                            end
+                        end,
+                    })
+                end,
+                labeler = function (matches, state)
+                    local labels = state:labels()
+
+                    for m, match in ipairs(matches) do
+                        match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                        match.label2 = labels[(m - 1) % #labels + 1]
+                        match.label = match.label1
+                    end
+                end,
+            })
+        end,
+        noremap = true,
+        silent = true,
+        desc = "flash all",
+    },
+    {
         ",fs",
         mode = { "n", "x", "o" },
         function ()
