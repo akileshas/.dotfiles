c40f6a1bf2e86247d4e3a1ee1ea66fc639c7fbdc
akileshas <akileshas.ai@gmail.com>
2025-06-04 09:44:25 +0530

    initialised the script!

--- setup/sys/install.sh
+++ setup/sys/install.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+
+
+#! autor            => akileshas
+#! created on       => 04-06-2025
+#! last modified on => 04-06-2025
+#! description      => system ( archlinux + i3wm + x11 ) setup script
+
+
+
+# starting the installation script
+echo "installing the required packages for my system..."
+
+# checking if `paru` is installed
+if ! command -v paru &> /dev/null; then
+    echo
+    echo "error: 'paru' is not installed !!!"
+    exit 1
+fi
+
+echo && echo "installling the required packages for my system... done. ;)"
