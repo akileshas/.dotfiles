142e2bac3d93f03e6e59cfc0b5f997031c4161c2
akileshas <akileshas.ai@gmail.com>
2025-06-04 11:36:28 +0530

    updated the script to installed the required pkgs!

--- setup/sys/init.sh
+++ setup/sys/init.sh
@@ -5,27 +5,39 @@
 #! autor            => akileshas
 #! created on       => 04-06-2025
 #! last modified on => 04-06-2025
-#! description      => system ( archlinux + i3wm + x11 ) setup script
+#! description      => my system (akileshas@ASA) setup script
 
 
 
-# obtaining `sudo` privileges access
+## declaring the global variables
+FILE_PATH="/home/$USER/.dotfiles/setup/sys/pkglist/pkgs.txt"
+
+## obtaining `sudo` privileges access
 sudo -v
 
-# starting the installation script
+## starting the installation script
 echo && echo "[#!](akileshas@ASA) setting up my system ..."
 
-# checking if `paru` is installed
-if ! command -v paru &> /dev/null; then
+## checking if `paru` is installed
+if [[ -z $(command -v paru) ]]; then
     echo && echo "[!!] error: 'paru' is not installed !!!"
     exit 1
 fi
 
-# updating and synchronizing the system
+## updating and synchronizing the system
 echo && echo "[::] updating and synchronizing the system ..."
 sudo pacman -Syu --noconfirm
 paru -Syu --noconfirm
 echo "[::] updating and synchronizing the system ... done."
 
-echo
-echo "[#!](akileshas@ASA) setting up my system ... done. ;)"
+## installing the required packages
+if [[ -f "$FILE_PATH" ]]; then
+    echo && echo "[::] installing the required packages ..."
+    mapfile -t pkgs < <(grep -vE '^\s*#|^\s*$' "$FILE_PATH")
+    if [[ ${#pkgs[@]} -gt 0 ]]; then
+        paru -S --noconfirm "${pkgs[@]}"
+    fi
+    echo "[::] installing the required packages ... done."
+fi
+
+echo && echo "[#!](akileshas@ASA) setting up my system ... done. ;)"
