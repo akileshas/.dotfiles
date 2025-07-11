18f5e80692eb5d19296ecb1f09729b5cb119709e
akileshas <akileshas.ai@gmail.com>
2025-07-11 23:25:56 +0530

    [chore]: update cp template

--- nvim.old/templates/basics.cpp
+++ nvim.old/templates/basics.cpp
@@ -47,8 +47,8 @@ void solve() {
 
 int main() {
 #ifndef ONLINE_JUDGE
-    freopen("/home/akileshas/personal/AlgoX/input.txt", "r", stdin);
-    freopen("/home/akileshas/personal/AlgoX/output.txt", "w", stdout);
+    freopen("/home/akileshas/repos/algoX/input.txt", "r", stdin);
+    freopen("/home/akileshas/repos/algoX/output.txt", "w", stdout);
 #endif
 
     ios_base::sync_with_stdio(0);
