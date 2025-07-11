1df7627d10b5efff83d2ad23c64062de63382793
akileshas <akileshas.ai@gmail.com>
2025-07-11 22:41:16 +0530

    [feat]: add cp methods

--- nvim/lua/akileshas/configs/init.lua
+++ nvim/lua/akileshas/configs/init.lua
@@ -2,5 +2,6 @@ require("akileshas.configs.globals")
 require("akileshas.configs.options")
 require("akileshas.configs.keymaps")
 require("akileshas.configs.autocmds")
+require("akileshas.configs.cp")
 
 return {}
